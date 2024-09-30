f0f60c2a7d1a1dd8cceac80dcba38efe9d79ba57
Aysel Bilmez <aybilmez@gmail.com>
2024-09-30 23:06:12 +0300

    Proje dosyası ekle.

---
+++ HelloPatika/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloPatika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aysel Bilmez");
+            Console.WriteLine("Alaninda cok iyi bir yazilimci olabilmek icin patika bootcampteyim");
+        }
+    }
+}
+
+
+
